0928b20ce8557338250cb78e25c9c104499ddf0e
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-05-16 14:20:51 -0300

    adicionando rota de envio de telemetria

---
+++ routes/post_telemetry.go
@@ -0,0 +1,38 @@
+package routes
+
+import (
+	"github.com/games4l/telemetria/providers"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
+
+func PostTelemetry(ts *providers.TelemetryService) func(c *fiber.Ctx) error {
+	validate := validator.New()
+
+	return func(c *fiber.Ctx) error {
+		telemetryData := providers.CreateTelemetryUnitData{}
+
+		c.BodyParser(&telemetryData)
+
+		err := validate.Struct(telemetryData)
+
+		if err != nil {
+			return c.JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		result, err := ts.Create(&telemetryData)
+
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "something went wrong",
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"message": "success",
+			"data":    result,
+		})
+	}
+}
