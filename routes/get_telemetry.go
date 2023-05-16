d72f5d7103ec0c085a8c53b7a019e6dacf749511
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-05-16 15:04:50 -0300

    adicionando rota para achar registros por id

---
+++ routes/get_telemetry.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"fmt"
+
+	"github.com/games4l/telemetria/providers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetTelemetryUnit(ts *providers.TelemetryService) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		idParam := c.Params("id")
+
+		item, err := ts.FindById(idParam)
+
+		if err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": fmt.Sprintf("telemetry registry %s could not be found", idParam),
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"message": "registry found",
+			"data": item,
+		})
+	}
+}
