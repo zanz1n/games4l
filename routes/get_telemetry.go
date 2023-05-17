89967db9b088ba8c32f101488c1cfc9528ab1129
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-05-17 20:53:42 -0300

    autenticação necessária para ver o nome do paciente

--- routes/get_telemetry.go
+++ routes/get_telemetry.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/games4l/telemetry-service/providers"
 	"github.com/gofiber/fiber/v2"
@@ -16,6 +17,23 @@ func GetTelemetryUnit(ts *providers.TelemetryService) func(c *fiber.Ctx) error {
 		if err != nil {
 			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 				"error": fmt.Sprintf("telemetry registry %s could not be found", idParam),
+			})
+		}
+
+		authHeaderS := strings.Split(c.Get("Authorization"), " ")
+
+		if len(authHeaderS) < 2 {
+			item.PacientName = "<OMITTED>"
+
+			return c.JSON(fiber.Map{
+				"message": "registry found",
+				"data":    item,
+			})
+		}
+
+		if authHeaderS[0] != "Bearer" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": fmt.Sprintf("invalid authorization stategy %s", authHeaderS[0]),
 			})
 		}
 
