1962c9ba58cdeae82ce663fb34a2a16209ed251c
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-05-19 22:13:39 -0300

    atualizando o schema [ ObjectID e validação ]

--- go.mod
+++ go.mod
@@ -6,8 +6,8 @@ require (
 	github.com/go-playground/validator/v10 v10.13.0
 	github.com/goccy/go-json v0.10.2
 	github.com/gofiber/fiber/v2 v2.45.0
-	github.com/matoous/go-nanoid/v2 v2.0.0
 	go.mongodb.org/mongo-driver v1.11.6
+	golang.org/x/text v0.8.0
 )
 
 require (
@@ -38,5 +38,4 @@ require (
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
 )
--- providers/data.go
+++ providers/data.go
@@ -2,14 +2,21 @@ package providers
 
 import (
 	"context"
+	"errors"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/games4l/telemetry-service/logger"
-	nanoid "github.com/matoous/go-nanoid/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
 )
+
+var normalizer = transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
 
 type CreateTelemetryUnitData struct {
 	DoneAt       int64  `json:"done_at,omitempty" validate:"required"`
@@ -20,14 +27,13 @@ type CreateTelemetryUnitData struct {
 }
 
 type TelemetryUnit struct {
-	ID           string `bson:"_id,omitempty" json:"id,omitempty" validate:"required"`
-	CreatedAt    int64  `bson:"created_at,omitempty" json:"created_at,omitempty" validate:"required"`
-
-	DoneAt       int64  `bson:"done_at,omitempty" json:"done_at,omitempty" validate:"required"`
-	CompleteTime int64  `bson:"complete_time,omitempty" json:"complete_time,omitempty" validate:"required"`
-	Answereds    []int8 `bson:"answereds,omitempty" json:"answered,omitempty" validate:"required"`
-	QuestionID   uint   `bson:"quest_id,omitempty" json:"quest_id,omitempty" validate:"required"`
-	PacientName  string `bson:"pacient_name,omitempty" json:"pacient_name,omitempty" validate:"required"`
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty" validate:"required"`
+	CreatedAt    primitive.DateTime `bson:"created_at,omitempty" json:"created_at,omitempty" validate:"required"`
+	DoneAt       primitive.DateTime `bson:"done_at,omitempty" json:"done_at,omitempty" validate:"required"`
+	CompleteTime int64              `bson:"complete_time,omitempty" json:"complete_time,omitempty" validate:"required"`
+	Answereds    []int8             `bson:"answereds,omitempty" json:"answered,omitempty" validate:"required"`
+	QuestionID   uint               `bson:"quest_id,omitempty" json:"quest_id,omitempty" validate:"required"`
+	PacientName  string             `bson:"pacient_name,omitempty" json:"pacient_name,omitempty" validate:"required"`
 }
 
 type TelemetryService struct {
@@ -55,7 +61,14 @@ func (ds *TelemetryService) FindById(id string) (tu *TelemetryUnit, err error) {
 
 	tu = &TelemetryUnit{}
 
-	err = ds.col.FindOne(ctx, bson.D{{Key: "_id", Value: id}}).Decode(tu)
+	oid, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		err = errors.New("invalid object id format")
+		return
+	}
+
+	err = ds.col.FindOne(ctx, bson.D{{Key: "_id", Value: oid}}).Decode(tu)
 
 	if err != nil {
 		logger.Error(err.Error())
@@ -73,25 +86,39 @@ func (ds *TelemetryService) Create(data *CreateTelemetryUnitData) (tu *Telemetry
 		return
 	}
 
+	for _, answred := range data.Answereds {
+		if answred > 4 || answred < 1 {
+			err = errors.New("invalid answered range")
+			return
+		}
+	}
+
+	if data.DoneAt < GetConfig().ProjectEpoch {
+		err = errors.New("invalid done_at prop: timestamp out of accepted range")
+		return
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
+	normalizedName, _, err := transform.String(normalizer, strings.ToLower(data.PacientName))
+
 	tu = &TelemetryUnit{
-		DoneAt:       data.DoneAt,
+		DoneAt:       primitive.NewDateTimeFromTime(time.UnixMilli(data.DoneAt)),
 		CompleteTime: data.CompleteTime,
 		Answereds:    data.Answereds,
 		QuestionID:   data.QuestionID,
-		PacientName:  strings.ToLower(data.PacientName),
+		PacientName:  normalizedName,
 	}
 
-	tu.ID, err = nanoid.New(18)
+	tu.ID = primitive.NewObjectID()
 
 	if err != nil {
 		logger.Error(err.Error())
 		return
 	}
 
-	tu.CreatedAt = time.Now().UnixMilli()
+	tu.CreatedAt = primitive.NewDateTimeFromTime(time.Now())
 
 	_, err = ds.col.InsertOne(ctx, tu)
 
