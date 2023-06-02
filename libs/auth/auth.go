73c4e550ee731363b69e27e7f618397aa15541bb
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-06-02 18:23:17 -0300

    adicionando funções jwt a lib de autenticação

--- libs/auth/auth.go
+++ libs/auth/auth.go
@@ -4,8 +4,12 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
+	"fmt"
+
 	"github.com/games4l/backend/libs/utils"
 	"github.com/games4l/backend/libs/utils/httpcodes"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 type ByteEncoding string
@@ -15,14 +19,101 @@ const (
 	ByteEncodingHex    ByteEncoding = "HEX"
 )
 
-type AuthProvider struct {
-	sigKey []byte
+type UserRole string
+
+const (
+	UserRolePacient UserRole = "PACIENT"
+	UserRoleAdmin   UserRole = "ADMIN"
+	UserRoleClient  UserRole = "CLIENT"
+)
+
+var ValidUserRoles = []UserRole{UserRoleAdmin, UserRoleClient, UserRolePacient}
+
+type JwtUserData struct {
+	Role     UserRole `json:"role"`
+	Username string   `json:"username"`
+	ID       string   `json:"id"`
 }
 
-func NewAuthProvider(sigKey []byte) *AuthProvider {
+type AuthProvider struct {
+	sigKey []byte
+	jwtKey []byte
+}
+
+func NewAuthProvider(sigKey []byte, jwtKey []byte) *AuthProvider {
 	return &AuthProvider{
 		sigKey: sigKey,
+		jwtKey: jwtKey,
 	}
+}
+
+func (ap *AuthProvider) AuthUser(payload string) (*JwtUserData, utils.StatusCodeErr) {
+	var (
+		valId string
+		valUsername string
+		valRole UserRole
+	)
+	token, err := jwt.Parse(payload, func(t *jwt.Token) (interface{}, error) {
+		var (
+			ok bool
+			claims jwt.MapClaims
+		)
+
+		formatErr := errors.New("invalid auth token metadata format, please sign-out ang log-in again")
+
+		if _, ok = t.Method.(*jwt.SigningMethodHMAC); !ok {
+			headerAlg, ok := t.Header["alg"].(string)
+			if !ok {
+				headerAlg = "<IMPOSSIBLE TO DECODE>"
+			}
+
+			return nil, fmt.Errorf("invalid auth token signing method: %s", headerAlg)
+		}
+
+		if claims, ok = t.Claims.(jwt.MapClaims); !ok {
+			return nil, formatErr
+		}
+
+		if valId, ok = claims["id"].(string); !ok {
+			return nil, formatErr
+		}
+
+		if valUsername, ok = claims["username"].(string); !ok {
+			return nil, formatErr
+		}
+
+		if valRole, ok = claims["role"].(UserRole); !ok {
+			return nil, formatErr
+		}
+
+		if !utils.SliceContains(ValidUserRoles, valRole) {
+			return nil, formatErr
+		}
+
+		return []byte(ap.jwtKey), nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, utils.NewStatusCodeErr(err.Error(), httpcodes.StatusUnauthorized)
+	}
+
+	return &JwtUserData{
+		ID: valId,
+		Role: valRole,
+		Username: valUsername,
+	}, nil
+}
+
+func (ap *AuthProvider) GenerateUserJwtToken(info JwtUserData) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+		"id":       info.ID,
+		"username": info.Username,
+		"role":     info.Role,
+	})
+
+	tokenEnc, err := token.SignedString(ap.jwtKey)
+
+	return tokenEnc, err
 }
 
 func (ap *AuthProvider) ValidateSignature(method ByteEncoding, body, givenBytes []byte) utils.StatusCodeErr {
--- libs/auth/go.mod
+++ libs/auth/go.mod
@@ -1,3 +1,5 @@
 module github.com/games4l/backend/libs/auth
 
 go 1.20
+
+require github.com/golang-jwt/jwt/v4 v4.5.0
