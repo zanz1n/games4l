f126f97535a1c801183296b7cdea28cab6467a9f
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-06-09 21:16:12 -0300

    movendo utilidades do fiber para lib separada

--- libs/logger/logger.go
+++ libs/logger/logger.go
@@ -5,9 +5,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"time"
-
-	"github.com/gofiber/fiber/v2"
 )
 
 func defaultLogger(out io.Writer, prefix ...string) *log.Logger {
@@ -24,45 +21,11 @@ func defaultLogger(out io.Writer, prefix ...string) *log.Logger {
 	)
 }
 
-func statusColor(code int, colors fiber.Colors) string {
-	switch {
-	case code >= fiber.StatusOK && code < fiber.StatusMultipleChoices:
-		return colors.Green
-	case code >= fiber.StatusMultipleChoices && code < fiber.StatusBadRequest:
-		return colors.Blue
-	case code >= fiber.StatusBadRequest && code < fiber.StatusInternalServerError:
-		return colors.Yellow
-	default:
-		return colors.Red
-	}
-}
-
-func methodColor(method string, colors fiber.Colors) string {
-	switch method {
-	case fiber.MethodGet:
-		return colors.Cyan
-	case fiber.MethodPost:
-		return colors.Green
-	case fiber.MethodPut:
-		return colors.Yellow
-	case fiber.MethodDelete:
-		return colors.Red
-	case fiber.MethodPatch:
-		return colors.White
-	case fiber.MethodHead:
-		return colors.Magenta
-	case fiber.MethodOptions:
-		return colors.Blue
-	default:
-		return colors.Reset
-	}
-}
-
 var (
 	stdOutLogger *log.Logger = defaultLogger(os.Stdout)
 	stdErrLogger *log.Logger = defaultLogger(os.Stderr)
 
-	config *Config
+	LoggerCfg *Config
 )
 
 type Config struct {
@@ -74,88 +37,42 @@ type Config struct {
 }
 
 func Init() {
-	config = &Config{}
+	LoggerCfg = &Config{}
 
-	config.InfoPrefix = "\tINFO\t"
-	config.ErrorPrefix = "\tERROR\n"
-	config.HttpPrefix = "\tHTTP\t"
-	config.WarningPrefix = "\tWARN\t"
+	LoggerCfg.InfoPrefix = "\tINFO\t"
+	LoggerCfg.ErrorPrefix = "\tERROR\n"
+	LoggerCfg.HttpPrefix = "\tHTTP\t"
+	LoggerCfg.WarningPrefix = "\tWARN\t"
 
 	if os.Getenv("TERM") == "dumb" || os.Getenv("NO_COLOR") == "1" {
-		config.Colors = false
+		LoggerCfg.Colors = false
 	} else {
-		config.Colors = true
-		config.InfoPrefix = "\x1b[36m" + config.InfoPrefix + "\x1b[0m"
-		config.WarningPrefix = "\x1b[33m" + config.WarningPrefix + "\x1b[0m"
-		config.ErrorPrefix = "\x1b[31m" + config.ErrorPrefix + "\x1b[0m"
-		config.HttpPrefix = "\x1b[34m" + config.HttpPrefix + "\x1b[0m"
+		LoggerCfg.Colors = true
+		LoggerCfg.InfoPrefix = "\x1b[36m" + LoggerCfg.InfoPrefix + "\x1b[0m"
+		LoggerCfg.WarningPrefix = "\x1b[33m" + LoggerCfg.WarningPrefix + "\x1b[0m"
+		LoggerCfg.ErrorPrefix = "\x1b[31m" + LoggerCfg.ErrorPrefix + "\x1b[0m"
+		LoggerCfg.HttpPrefix = "\x1b[34m" + LoggerCfg.HttpPrefix + "\x1b[0m"
 	}
 }
 
 func Info(format string, args ...any) {
-	stdOutLogger.Output(2, fmt.Sprintf(config.InfoPrefix+format+"\n", args...))
+	stdOutLogger.Output(2, fmt.Sprintf(LoggerCfg.InfoPrefix+format+"\n", args...))
 }
 
 func Warn(format string, args ...any) {
-	stdOutLogger.Output(2, fmt.Sprintf(config.WarningPrefix+format+"\n", args...))
+	stdOutLogger.Output(2, fmt.Sprintf(LoggerCfg.WarningPrefix+format+"\n", args...))
 }
 
 func Error(format string, args ...any) {
-	stdErrLogger.Output(2, fmt.Sprintf(config.ErrorPrefix+format+"\n", args...))
+	stdErrLogger.Output(2, fmt.Sprintf(LoggerCfg.ErrorPrefix+format+"\n", args...))
 }
 
 func Http(format string, args ...any) {
-	stdOutLogger.Output(2, fmt.Sprintf(config.HttpPrefix+format, args...))
+	stdOutLogger.Output(2, fmt.Sprintf(LoggerCfg.HttpPrefix+format, args...))
 }
 
 func Fatal(args ...any) {
-	args = append([]any{config.ErrorPrefix}, args...)
+	args = append([]any{LoggerCfg.ErrorPrefix}, args...)
 	stdErrLogger.Println(args...)
 	os.Exit(1)
-}
-
-func NewFiberMiddleware() fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		start := time.Now()
-
-		chainErr := c.Next()
-
-		errHandler := c.App().ErrorHandler
-
-		if chainErr != nil {
-			Error("%v", chainErr)
-			if err := errHandler(c, chainErr); err != nil {
-				_ = c.SendStatus(fiber.StatusInternalServerError)
-			}
-		}
-
-		end := time.Now()
-
-		if config.Colors {
-			Http(
-				"[%s]:%s  %s%s\x1b[0m  %s  %s%v\x1b[0m  %s%v\x1b[0m",
-				c.IP(),
-				c.Port(),
-				methodColor(c.Method(), fiber.DefaultColors),
-				c.Method(),
-				c.Path(),
-				statusColor(c.Response().StatusCode(), fiber.DefaultColors),
-				c.Response().StatusCode(),
-				"\x1b[90m",
-				end.Sub(start),
-			)
-		} else {
-			Http(
-				"[%s]:%s  %s  %s  %v  %v",
-				c.IP(),
-				c.Port(),
-				c.Method(),
-				c.Path(),
-				c.Response().StatusCode(),
-				end.Sub(start),
-			)
-		}
-
-		return nil
-	}
 }
