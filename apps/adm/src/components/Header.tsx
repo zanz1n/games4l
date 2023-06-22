c073231f191a364573d6059bdc74e97dbfe51797
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-06-22 18:50:08 -0300

    atualização que não afeta o funcionamento

--- apps/adm/src/components/Header.tsx
+++ apps/adm/src/components/Header.tsx
@@ -18,29 +18,28 @@ export default function Header() {
                     </Link>
                 </div>
                 <div className={styles.right}>
-                    {
-                        isLoggedIn() ? (
-                            <>
-                                <nav className={styles.nav}>
-                                    <Link to="/">Home</Link>
-                                    <Link to="/dash/questions">Perguntas</Link>
-                                    <Link to="/dash/users">Usuários</Link>
-                                </nav>
-                                <button onClick={() => {
-                                    logOut();
-                                    navigate("/auth/login");
-                                }} className={styles.purple}>Logout</button>
-                                <div className={styles.sideUsername}>
-                                    <p>Logged in as</p>
-                                    <p>{user?.username}</p>
-                                </div>
-                            </>
-                        ) : (
-                            <>
-                                <button onClick={() => navigate("/auth/login")} className={styles.green}>Sign In</button>
-                            </>
-                        )
-                    }
+                    {isLoggedIn() ? (
+                        <>
+                            <nav className={styles.nav}>
+                                <Link to="/">Home</Link>
+                            </nav>
+                            <button onClick={() => {
+                                logOut();
+                                navigate("/auth/login");
+                            }} className={styles.purple}>Logout</button>
+                            <div className={styles.sideUsername}>
+                                <p>Logged in as</p>
+                                <p>{user?.username}</p>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <button 
+                                onClick={() => navigate("/auth/login")}
+                                className={styles.green}>Sign In
+                            </button>
+                        </>
+                    )}
                 </div>
             </header>
         </div>
