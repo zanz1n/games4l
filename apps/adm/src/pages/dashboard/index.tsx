a4a9e8e35b5d02b2790f7c614246ce33376079e2
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-06-22 19:10:17 -0300

    atualizando componente das páginas da dashboard

--- apps/adm/src/pages/dashboard/index.tsx
+++ apps/adm/src/pages/dashboard/index.tsx
@@ -1,7 +1,49 @@
+import { useNavigate } from "react-router-dom";
+import DashMenuItem from "../../components/DashMenuItem";
 import Header from "../../components/Header";
+import styles from "./index.module.css";
+import { useAuth } from "../../lib/Auth";
+import { useEffect } from "react";
 
-export default function DashBoardMain() {
+export interface DashBoardProps {
+    route: string;
+}
+
+export default function DashBoardMain({ route }: DashBoardProps) {
+    const { isLoggedIn } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isLoggedIn()) {
+            navigate("/auth/login");
+        }
+    }, []);
+
     return <>
         <Header/>
+        <div className={styles.main}>
+            <div className={styles.dashMenuContainer}>
+                <div className={styles.menu}>
+                    <DashMenuItem
+                        to="/dash/create-question"
+                        icon="/pencil.png"
+                        text="Criar Pergunta"
+                    />
+                    <DashMenuItem
+                        to="/dash/edit-question"
+                        icon="/magnifier.png"
+                        text="Editar Pergunta"
+                    />
+                </div>
+            </div>
+            <main className={styles.dashBody}>
+                {
+                    route == "create-question"
+                        ?
+                        ""
+                        : ""
+                }
+            </main>
+        </div>
     </>;
 }
