ae865355dac486cfe60f03c5949bd731964e783e
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-09-02 22:23:51 -0300

    adding web svelte app

---
+++ apps/web/src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  let counter = 1;
+
+  function setCounter(n: number) {
+    counter = n;
+  }
+</script>
+
+<main>
+  <h1>Hello World</h1>
+  <p>Counter: {counter}</p>
+  <button
+    on:click={() => {
+      setCounter(counter - 1);
+    }}>Decrement</button
+  >
+  <button
+    on:click={() => {
+      setCounter(counter + 1);
+    }}>Increment</button
+  >
+</main>
+
+<style>
+</style>
---
+++ apps/web/src/main.ts
@@ -0,0 +1,6 @@
+import "./global.css";
+import App from "./App.svelte";
+
+export default new App({
+    target: document.getElementById("root")!,
+});
