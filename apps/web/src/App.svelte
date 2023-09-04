0b0c18ccdf48384f8754396e49790912bd330157
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-09-03 22:36:33 -0300

    adicionando página de novo jogo

--- apps/web/src/App.svelte
+++ apps/web/src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import { Router, Route } from "svelte-routing";
   import Home from "./pages/Home.svelte";
+  import NewGame from "./pages/NewGame.svelte";
 
   export let url = "";
 </script>
 
 <Router {url}>
   <Route path="/" component={Home} />
+  <Route path="/new-game" component={NewGame} />
 </Router>
---
+++ apps/web/src/pages/NewGame.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import { navigate } from "svelte-routing";
+  import sharedStyles from "../shared.module.css";
+
+  function onSubmit() {
+    const name = (document.getElementById("name") as HTMLInputElement).value;
+    if (1 > name.length) {
+      return alert("Insira um nome válido!");
+    }
+
+    const age = (document.getElementById("age") as HTMLInputElement)
+      .valueAsNumber;
+    if (isNaN(age) || 1 > age) {
+      return alert("Insira uma idade válida!");
+    }
+  }
+</script>
+
+<div class={sharedStyles.container}>
+  <div class="center top">
+    <div class="header">
+      <button
+        on:click={() => {
+          navigate("/");
+        }}
+        class={`${sharedStyles.btn} btn-back`}>Voltar</button
+      >
+    </div>
+    <img height="192px" src="/new-game.png" alt="New Game" />
+  </div>
+
+  <div class="main center">
+    <div class="title">
+      <h1>Identificação do paciente</h1>
+    </div>
+
+    <input id="name" type="text" placeholder="Nome" />
+    <input id="age" type="number" placeholder="Idade" />
+  </div>
+
+  <div class="center">
+    <button on:click={onSubmit} class={`${sharedStyles.btn} btn-start`}
+      >Prosseguir</button
+    >
+  </div>
+</div>
+
+<style>
+  .btn-back {
+    background-color: var(--cyan);
+    font-size: 24px;
+    width: 100px;
+    height: 36px;
+  }
+
+  .btn-start {
+    background-color: var(--cyan);
+    width: 70%;
+    min-width: 280px;
+    height: 68px;
+    font-size: 38px;
+    margin-top: 24px;
+  }
+
+  .center {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .top {
+    gap: 32px;
+  }
+
+  .top .header {
+    width: 100%;
+  }
+
+  .main {
+    gap: 16px;
+  }
+
+  .main .title {
+    text-align: center;
+  }
+
+  .main input {
+    height: 52px;
+    width: 60%;
+    min-width: 280px;
+    font-size: 24px;
+    padding: 0 8px;
+  }
+
+  .main .title {
+    margin-bottom: 24px;
+  }
+</style>
