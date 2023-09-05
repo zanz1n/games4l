402968d2a25eb1b7fdcb3c34c0973e065b14a4c1
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-09-05 20:11:05 -0300

    adicionando suporte a pwa

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,53 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { imagetools } from "vite-imagetools";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte(), imagetools()],
+    plugins: [
+        svelte(),
+        imagetools(),
+        VitePWA({
+            manifest: {
+                name: "Memories for Life",
+                short_name: "Memories4Life",
+                description: "Um jogo desenvolvido pelo Colégio Poliedro em parceria com o Hospital Reger",
+                lang: "pt-BR",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "favicon.png",
+                        sizes: "128x128",
+                        type: "image/png"
+                    },
+                    {
+                        src: "favicon-big.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    },
+                    {
+                        src: "favicon-big.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any"
+                    },
+                    {
+                        src: "favicon-big.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable"
+                    }
+                ]
+            },
+            registerType: "autoUpdate",
+            injectRegister: "inline",
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,ico,png,svg,gif,webp,avif,json}"],
+                inlineWorkboxRuntime: true,
+            }
+        })
+    ],
     build: {
         target: "esnext"
     }
